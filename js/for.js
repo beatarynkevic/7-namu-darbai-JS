e74f744ad77ceab6fd788e761b8383bf91a6b6bf
beatarynkevic <beatainn@gmail.com>
2020-11-12 21:07:27 +0200

    namu darbai

--- js/for.js
+++ js/for.js
@@ -43,6 +43,56 @@ const miestai = ['Vilnius', 'Kaunas', 'Klaipeda', 'Anyksciai'];
 // console.log(`Aplankiau: ${miestai[3]}.`);
 
 
+for (let i = 3; i>=0; i--) {
+    console.log(`Aplankiau: ${miestai[i]}`);
+}
+
 for (let i = 0; i<4; i++) {
     console.log(`Aplankiau: ${miestai[i]}`);
 }
+
+for (let i = 0; i < miestai.length; i++) {
+     const miestoPavadinimas = miestai[i];
+     console.log(`Aplankiau: ${miestoPavadinimas}`);
+}
+ 
+
+const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+console.log(abc);
+
+for (let pozicija = abc.length - 1; pozicija >= 0; pozicija--) {
+    console.log(abc[pozicija]);
+}
+
+console.clear();
+
+const text = 'dodekahedronas';
+
+console.log(text[0]);
+console.log(text[1]);
+console.log(text[2]);
+
+console.log(text.length);
+
+for (let i = 0; i < text.length; i++) {
+    console.log(text[i]);
+}
+
+console.clear();
+//kas antra raide isspausdinti -> bdf
+
+const text2 = 'abcdef';
+let rez = '';
+
+for (let i = 1; i < text2.length; i = i + 2) {
+    const raid = text2[i];
+    rez = rez + raid;
+}
+
+
+console.log(rez);
+
+
+// 0 1 2 3 4 5
+// a b c d e f
