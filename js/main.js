fde089b45db2b9abe9c4ce9c61c982459752aaa9
beatarynkevic <beatainn@gmail.com>
2020-11-12 10:10:13 +0200

    more code

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+
+const age = 99;
+console.log(age);
+
+const year = 2020;
+console.log(year);
+
+const luckyNumber = 13;
+console.log(luckyNumber);
+
+const name = 'Johny';
+console.log(name);
+
+const city = 'Abu Dabi';
+console.log(city);
+
+const name = 'Johny';
+console.log(name);
